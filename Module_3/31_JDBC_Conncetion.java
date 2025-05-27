4f87a7e13a20aed741d5f648b3455a771159f07d
SwathiBurgala <swathiburgala.nsn07@gmail.com>
2025-05-27 10:56:30 +0530

    Create 31_JDBC_Conncetion.java

---
+++ Module_3/31_JDBC_Conncetion.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class JDBCConnect {
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:students.db");
+             Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+            while (rs.next())
+                System.out.println(rs.getInt("id") + ": " + rs.getString("name"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
