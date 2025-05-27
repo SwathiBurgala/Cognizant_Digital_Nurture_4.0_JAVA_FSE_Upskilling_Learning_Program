9c16ecd8b62aaeb3d53e904607f15bbcc15be34a
SwathiBurgala <swathiburgala.nsn07@gmail.com>
2025-05-27 10:48:19 +0530

    Create 22_File_Writing.java

---
+++ Module_3/22_File_Writing.java
@@ -0,0 +1,19 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileWrite {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter text: ");
+            String text = sc.nextLine();
+            FileWriter writer = new FileWriter("output.txt");
+            writer.write(text);
+            writer.close();
+            System.out.println("Written to file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
