aa244a731f4461783da40901842b2f9d48b23ed8
SwathiBurgala <swathiburgala.nsn07@gmail.com>
2025-05-27 12:32:34 +0530

    Create 2.js

---
+++ Module_1/JavaScript/2.js
@@ -0,0 +1,5 @@
+const eventName = "Community Cleanup";
+const eventDate = "2025-06-15";
+let seatsAvailable = 50;
+console.log(`${eventName} is scheduled for ${eventDate}. Seats left: ${seatsAvailable}`);
+seatsAvailable--;
